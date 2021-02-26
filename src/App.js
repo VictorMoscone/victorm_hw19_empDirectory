362f98012824856247265f705aa973b8a8e82c6d
Victor Moscone <victormoscone@gmail.com>
2021-02-25 21:40:53 -0800

    Refactor: Undoing previous change. It wasn't working.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,36 @@ import Table from 'react-bootstrap/Table'
 import 'bootstrap/dist/css/bootstrap.css';
 import TableBody from './components/Table/TableBody';
 import TableHead from './components/Table/TableHead';
+import axios from "axios";
+import { useEffect, useState } from 'react';
 
 function App() {
+  
+  const getRequest =  async () => {
+    const URL = "https://randomuser.me/api/";
+
+    try{
+        const response = await axios.get(URL)
+        // console.log(response.data.results[0])
+        return {
+            fName: response.data.results[0].name.first, 
+            lName: response.data.results[0].name.last,
+            gender: response.data.results[0].gender,
+            email: response.data.results[0].email,
+        };
+        // console.log(fName)
+    } catch (err) {
+        console.log(err);
+    };
+  };
+
+  useEffect(() => {
+    // getRequest().then((res) => console.log(res.fName))
+    for (let i = 0; i < 4; i++) {
+      getRequest().then((res) => userArray.push(res));
+      console.log(userArray)
+    };
+  }, []);
 
   return (
     <div className="App">
