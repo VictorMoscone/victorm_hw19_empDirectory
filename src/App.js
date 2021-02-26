b2bf7979ea639ddcde640e8980d4bfaf4f3a47ef
Victor Moscone <victormoscone@gmail.com>
2021-02-25 19:30:13 -0800

    Feat / Refactor: Moved the API Get Request to App.js. On page load, it successfully runs the fetch any number of times for a new user.

--- src/App.js
+++ src/App.js
@@ -5,12 +5,41 @@ import Table from 'react-bootstrap/Table'
 import 'bootstrap/dist/css/bootstrap.css';
 import TableBody from './components/Table/TableBody';
 import TableHead from './components/Table/TableHead';
+import axios from "axios";
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  let [apiData, setApiData] = useState("");
+
+  const getRequest =  async (e) => {
+    const URL = "https://randomuser.me/api/";
+
+    try{
+        const response = await axios.get(URL)
+        console.log(response.data.results[0])
+        setApiData({ 
+            fName: response.data.results[0].name.first, 
+            lName: response.data.results[0].name.last,
+            gender: response.data.results[0].gender,
+            email: response.data.results[0].email,
+        });
+    } catch (err) {
+        console.log(err);
+    };
+  };
+
+  useEffect(() => {
+    for (let i = 0; i < 14; i++) {
+      getRequest();
+      console.log(apiData);
+    };
+  }, [])
+
+
   return (
     <div className="App">
       <Form formName="Filter" />
-      {/* <BSTable /> */}
       <Table striped="true" bordered="true" hover="true" responsive="md">
         <TableHead />
         <TableBody fName="Victor" lName="Moscone" gender="Male" email="test@test.com"/>
